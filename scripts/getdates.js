831b23b7d82f8e3ae114761afd756e459a4f9273
Sheldon Medeiros <shizzmede13@gmail.com>
2023-09-16 13:39:57 -0600

    lesson number 1

---
+++ scripts/getdates.js
@@ -0,0 +1,22 @@
+function getCurrentYear() {
+    const currentYear = new Date().getFullYear();
+    return currentYear;
+}
+
+function updateFooter() {
+    const copyrightYearElement = document.getElementById("copyrightYear");
+    const lastModifiedElement = document.getElementById("lastModified");
+
+    if (copyrightYearElement) {
+        copyrightYearElement.textContent = getCurrentYear();
+    }
+
+    if (lastModifiedElement) {
+        lastModifiedElement.textContent = formatLastModifiedDate();
+    }
+}
+
+
+
+document.getElementById("weather").textContent = "Partly Cloudy";
+document.getElementById("visits").textContent = "25";
